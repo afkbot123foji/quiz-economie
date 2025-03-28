fd04da3068b81c1b6c3f29e011f186c0873ecf62
afkbot123foji <78604085+afkbot123foji@users.noreply.github.com>
2025-03-28 14:53:34 +0000

    Delete website-project directory

--- website-project/src/main.js
+++
@@ -1,9 +0,0 @@
-import { initOpeningAnimation } from './animations/opening.js';
-import { Quiz } from './quiz/quiz.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    initOpeningAnimation();
-
-    const quiz = new Quiz();
-    quiz.startQuiz();
-});
